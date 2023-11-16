a8bf92a89332ab0dc4f1bf054dad08f0495292f4
Amansingh293 <amansinghdl9c@gmail.com>
2023-11-16 19:35:01 +0530

    4th commit

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -89,7 +89,7 @@ const Watchlist = () => {
             <button
               id="dropdownDefaultButton"
               data-dropdown-toggle="dropdown"
-              className="flex justify-between w-[5rem] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="w-[auto] gap-4 inline-flex justify-between text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               type="button"
               onClick={() => (isOpen ? setIsOpen(false) : setIsOpen(true))}
               ref={genreButtonRef}
